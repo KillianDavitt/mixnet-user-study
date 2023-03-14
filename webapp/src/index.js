783ac102f7f37ea4643b590c47acc79469750ae6
Killian <davittk@tcd.ie>
2023-03-14 17:39:23 +0000

    refactoring the user script

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -6,9 +6,11 @@ let sdoc = Automerge.init()
 
 
 pdoc = Automerge.change(pdoc, 'Add card', pdoc => {
-    pdoc.q1=''
-    pdoc.q2=''
-    pdoc.q3=''
+    pdoc.q=Array(8).fill('')
+})
+
+sdoc = Automerge.change(sdoc, 'Add card', doc => {
+    doc.q=Array(8).fill('')
 })
 
 
@@ -16,103 +18,141 @@ pdoc = Automerge.change(pdoc, 'Mark card as done', pdoc => {
   pdoc.test = 'hi'
 })
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+const answers = ["Vienna","London","Paris", "Madrid", "Berlin", "Rome", "Dublin", "Moscow"]
+
+let copyOfAnswers = answers.map(s => Array.from(s))
+const copy = copyOfAnswers.map(x => x)
+
+/*
+const updateValue = (pick = getRandomInt(letters.length), input = document.getElementById(`answer${pick}`)) => {
+    // This delay simulates typing delay, not network delay
+    console.log("Started update value")
+    let delay = poissonProcess.sample(500)
+    let id;
+    id = copy.indexOf(letters[pick])
+    console.log(id)
+    console.log(pick)
+    if (letters[pick].length == 0) {
+	console.log("in if")
+	letters.splice(pick, 1)
+	delay+=1000
+	if (letters.length === 0) {
+	    return
+	}
+	
+	do {
+	    pick = getRandomInt(letters.length)
+	    id = copy.indexOf(letters[pick])
+	    input = document.getElementById(`answer${id}`)
+	    if (sdoc.q[id]!=''){
+		letters.splice(pick,1)
+	    }
+	} while (input.value!='')
+    }
+
+    const letter = letters[pick].splice(0, 1)
+    console.log(letter)
+    setTimeout(() => {
+	
+	sdoc = Automerge.change(sdoc, 'Add letter', doc => {
+	    doc.q[id]+= letter
+	})
+
+	// Network delay
+	let delay = poissonProcess.sample(5000)
+	// Merge CRDTs
+	setTimeout(() => {
+	    pdoc = Automerge.merge(pdoc, sdoc)
+	    input.value = pdoc.q[id]
+	}, delay)
+	
+	updateValue(pick, input)
+    }, delay)
+
+}*/
+
+const updateValue = (strings = copy, pick = getRandomInt(strings.length), input = document.getElementById(`answer${pick}`)) => {
+    // This delay simulates typing delay, not network delay
+    let delay = poissonProcess.sample(500)
+    let id;
+    id = copyOfAnswers.indexOf(strings[pick])
+   
+    if (strings[pick].length == 0) {
+
+	strings.splice(pick, 1)
+	delay+=1000
+	if (strings.length === 0) {
+	    return
+	}
+	
+	do {
+	    pick = getRandomInt(strings.length)
+	    id = copyOfAnswers.indexOf(strings[pick])
+	    input = document.getElementById(`answer${id}`)
+	    if (sdoc.q[id]!=''){
+		strings.splice(pick,1)
+	    }
+	} while (sdoc.q[id]!='')
+    }
+
+    const letter = strings[pick].splice(0, 1)
+    console.log(letter)
+    setTimeout(() => {
+	
+	sdoc = Automerge.change(sdoc, 'Add letter', doc => {
+	    doc.q[id]+= letter
+	})
+
+	// Network delay
+	let delay = poissonProcess.sample(5000)
+	// Merge CRDTs
+	setTimeout(() => {
+	    pdoc = Automerge.merge(pdoc, sdoc)
+	    input.value = pdoc.q[id]
+	}, delay)
+	let stringsCopy = strings.map(s => Array.from(s))
+	updateValue(stringsCopy, pick, input)
+    }, delay)
+
+}
+
+
+
 document.querySelector('#testbox').value=pdoc.test
 
-function addItem(text) {
-  let newDoc = Automerge.change(doc, doc => {
-    if (!doc.items) doc.items = []
-    doc.items.push({ text, done: false })
-  })
-    updateDoc(newDoc)
-    console.log(text)
-}
-
-let form = document.querySelector("form")
-let input = document.querySelector("#new-todo")
-form.onsubmit = (ev) => {
-  ev.preventDefault()
-  addItem(input.value)
-  input.value = null
-}
-const numQuestions = 5;
+const numQuestions = 7;
 
 for (var i=0; i<numQuestions;i++){
-    let typeInput = document.querySelector(`answer${i}`)
-    typeInput.onkeydown = userTypes()
+    let typeInput = document.getElementById(`answer${i}`)
+    console.log(`answer${i}`)
+    console.log(pdoc.q[i])
+    //typeInput.onkeydown = updateValue()
+    typeInput.addEventListener( "keydown" ,
+			   (e) => {
+			       console.log(e.target)
+			       console.log(e.key)
+			       // Add leter to crdt
+			       pdoc = Automerge.change(pdoc, 'Add letter', doc => {
+				   doc.q[i]+=e.key
+			       })
+			       console.log(pdoc.q[i])
+			       // Delay
+			       let delay = poissonProcess.sample(1000)
+			       // Merge CRDTs
+			       setTimeout(() => {
+				   sdoc = Automerge.merge(sdoc, pdoc)
+			       }, delay)
+			   })
 }
-
-function userTypes(event){
-    console.log(event.target)
-    // Add leter to crdt
-    pdoc = Automerge.change(pdoc, 'Add letter', doc => {
-	doc.q[qnum]+=letter
-    })
-
-    // Delay
-    let delay = poissonProcess.sample(1000)
-    // Merge CRDTs
-    setTimeout(() => {
-	sdoc = Automerge.merge(sdoc, pdoc)
-    }, delay)
- 
-}
-
-/*function render(doc) {
-  let list = document.querySelector("#todo-list")
-  list.innerHTML = ''
-  doc.items && doc.items.forEach((item, index) => {
-    let itemEl = document.createElement('li')
-    itemEl.innerText = item.text
-    itemEl.style = item.done ? 'text-decoration: line-through' : ''
-    list.appendChild(itemEl)
-  })
-}
-*/
 
 function updateDoc(newDoc) {
   doc = newDoc
   render(newDoc)
 }
-//let questionBox = document.querySelector("#answer")
-
-function getRandomInt(max = 4) {
-  return Math.floor(Math.random() * max);
-}
-
-const strings = ["Vienna","London","Paris", "Madrid", "Berlin", "Rome", "Dublin", "Moscow"]
-
-const letters = strings.map(s => Array.from(s))
-const copy = letters.map(x => x)
-
-
-const updateValue = (pick = getRandomInt(letters.length), input = document.getElementById(`answer${pick}`)) => {
-    let delay = poissonProcess.sample(500)
-
-  if (letters[pick].length == 0) {
-      letters.splice(pick, 1)
-      delay+=1000
-    if (letters.length === 0) {
-      return
-    }
-      do {
-    pick = getRandomInt(letters.length)
-    const id = copy.indexOf(letters[pick])
-	  input = document.getElementById(`answer${id}`)
-	  if (input.value!=''){
-	      letters.splice(pick,1)
-	  }
-      } while (input.value!='')
-  }
-
-  const letter = letters[pick].splice(0, 1)
-
-  setTimeout(() => {
-    input.value += letter
-    updateValue(pick, input)
-  }, delay)
-
-}
-
 
 
 let startInput = document.querySelector("#start")
@@ -124,7 +164,7 @@ startInput.onclick = (ev) => {
 
 
 function clearAll(){
-    for (var i=0; i<strings.length;i++){
+    for (var i=0; i<answers.length;i++){
 	document.getElementById(`answer${i}`).value='';
     }
 }
@@ -135,45 +175,3 @@ clearInput.onclick = (ev) => {
     clearAll()
 }
 
-
-
-/*
-function startUser(){
-    const answers = ["Vienna", "London", "Paris", "Madrid", "Berlin", "Rome", "Dublin", "Moscow"]
-
-    let letters = answers.map(s => Array.from(s))
-    const copy = letters.map(x => x)
-
-    function getRandomInt(max) {
-	return Math.floor(Math.random() * max);
-    }
-    // Dan Ristea wrote this function
-    const typeAnswer = (boxIndex = getRandomInt(letters.length), inputBox = document.getElementById(`answer${boxIndex}`)) => {
-
-	var delay = poissonProcess.sample(500);
-
-	if (letters[boxIndex].length == 0) {
-	    letters.splice(boxIndex, 1)
-	    if (letters.length === 0) {
-		return
-	    }
-	    boxIndex = getRandomInt(letters.length)
-	    const id = copy.indexOf(letters[boxIndex])
-	    inputBox = document.getElementById(`answer${id}`)
-	
-	}
-
-	const letter = letters[boxIndex].splice(0, 1)
-
-    
-    setTimeout(() => {
-	inputBox.val+=letter;
-	typeAnswer(boxIndex,inputBox)
-    }, 500);
-}
-
-    
-   	typeAnswer()
-   
-}
-*/
